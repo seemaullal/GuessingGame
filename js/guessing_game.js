dd767277a092964e377046eae32fe8c32dda921d
Seema <seemaullal@gmail.com>
2015-01-05 14:43:54 -0800

    Added form functionality, kept track of guesses

--- js/guessing_game.js
+++ js/guessing_game.js
@@ -1,18 +1,37 @@
 var guessForm = $('#guessForm');
-var hintSection= $('#hints');
+//var hintSection= $('#hints');
 var instruct = $('#instructions');
 
+var maxGuesses = 5 ;
+var remGuesses = 5 ;
+var targetNum;
+var guessedNums= [ ];
+
 function guessEntered(event) {
-	console.log("Function called");
 	event.preventDefault();
-	instruct.hide();
-	//instruct.text("Changed");
-	//instruct.css("display","block")
-	hintSection.show();
+	remGuesses = remGuesses-1;
+	console.log(remGuesses);
+	if (remGuesses < 1 ) {
+		instruct.text("Game over. :( ");
+		$('#guess').disable();
+
+	}
+	else{
+		instruct.text("You have " + remGuesses + " guesses remaining.");
+	}
+	
+	//hintSection.show();
 }
 
 
 guessForm.submit(guessEntered);
 
+function playGame() {
+	//hintSection.hide();
+	guessedNums = [ ]; //reset guesses array (in case game played previously)
+	targetNum = Math.floor((Math.random()*100)+1); //generate a number between 0 and 100; add 1 so it is between 1 and 100; round down so it is an integer
+	remGuesses = maxGuesses; //reset # of remaining guesses
 
-hintSection.hide();
+}
+
+playGame();
