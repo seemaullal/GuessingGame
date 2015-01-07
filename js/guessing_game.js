e6f13b31374ff06c900f4bb1f93aa1d3e0b421d0
Seema <seemaullal@gmail.com>
2015-01-07 00:37:31 -0800

    Previous guesses listed and background changes on win. Need to add more detailed hints and input validation

--- js/guessing_game.js
+++ js/guessing_game.js
@@ -21,15 +21,30 @@ function guessEntered(event) {
 	remGuesses = remGuesses-1;
 	guessedNums.push(curGuess);
 	console.log(guessedNums);
-	if (remGuesses < 1 ) {
-		instruct.text("Game over. :( ");
+	if (curGuess!= targetNum) {
+		if (remGuesses < 1 ) {
+			instruct.text("Game over. :( The number was " + targetNum + ".");
+			$('#submitGuess').attr('disabled','disabled');
+			guess.attr('disabled','disabled');
+			hint.text('');
+			//$('#guessList').html('');
+		}
+		else{
+			checkGuess(curGuess);
+			instruct.text("You have " + remGuesses + " guesses remaining.");
+		}
+	}
+	else {
+		$('#img').attr('background','http://thumbs.dreamstime.com/z/party-streamers-confetti-background-23573239.jpg')
+		hint.text("Good job! You guessed the number.");
 		$('#submitGuess').attr('disabled','disabled');
-		guess.bind('keypress',noEnterSubmit )
+		guess.attr('disabled','disabled');
+		$('#guessList').html('');
+
+		//guess.bind('keypress',noEnterSubmit )
+		instruct.text('');
 	}
-	else{
-		checkGuess(curGuess);
-		instruct.text("You have " + remGuesses + " guesses remaining.");
-	}
+	
 	
 	//hintSection.show();
 }
@@ -37,17 +52,20 @@ function guessEntered(event) {
 function checkGuess(guessedNum) {
 	
 	if (guessedNum==targetNum) {
-		hint.text("Good job! You guessed the number.");
-		$('#submitGuess').attr('disabled','disabled');
-		guess.bind('keypress',noEnterSubmit )
+		return true;
 	}
 
 	else if (guessedNum < targetNum) {
+		$('#guessList').append('<li><span>You guessed ' + guessedNum + ' but it was too low. Guess higher!</span></li>');
 		hint.text("You are cold. Guess higher.");
+		return false
 	}
 
-	else 
+	else {
 		hint.text("You are hot. Guess lower.");
+		$('#guessList').append('<li>You guessed ' + guessedNum + ' but it was too high. Guess lower!</li>');
+		return true;
+	}
 
 }
 
@@ -56,9 +74,12 @@ reset.click(playGame);
 
 
 function playGame() {
+	$('#img').attr('background',"http://wallpaper-download.net/wallpapers/funny-wallpapers-cute-green-background-wallpaper-36415.jpg")
 	guess.val('');
 	hint.text('');
+	$('#guessList').html('');
 	$('#submitGuess').removeAttr('disabled');
+	guess.removeAttr('disabled');
 	guess.unbind('keypress',noEnterSubmit);
 	instruct.text("I am thinking of a number between 1 and 100  ...  you have five tries to guess it. Good luck!");
 	guessedNums = [ ]; //reset guesses array (in case game played previously)
